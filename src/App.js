8ededfd607a3a6401cfe4a8358f33156ac38c667
soggy2012 <adam.janathan@gmail.com>
2025-08-15 23:45:44 +0200

    Finish CodeCademy React-Router practice

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import Root from './components/root';
 
 // Add react-router-dom imports
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import Search from './components/search';
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={ <Root />}/>
+    <Route path="/" element={ <Root />}>
+      <Route index element ={ <HomePage />} />
+      <Route path=":type" element ={ <HomePage />} />
+      <Route path=":type/:id" element ={ <PetDetailsPage />} />
+      <Route path="search" element ={ <SearchPage />} />
+      <Route path="pet-details-not-found" element ={ <PetDetailsNotFound />} />
+    </Route>
 ))
 
 function App() {
--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -3,13 +3,14 @@ import { getPetDetails } from '../../api/petfinder';
 import Hero from '../../components/hero';
 
 // Import useParams
+import { useParams, Navigate } from 'react-router-dom';
 // Import Navigate
 
 const PetDetailsPage = () => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const id = '51322435'; // <--- Update me!
+  const { id } = useParams();
 
   useEffect(() => {
     async function getPetsData() {
@@ -33,6 +34,7 @@ const PetDetailsPage = () => {
       ) : error ? (
         <div>
           {/* Redirect to /pet-details-not-found if there was an error! */}
+          <Navigate to="/pet-details-not-found" />
         </div>
       ) : (
         <main>

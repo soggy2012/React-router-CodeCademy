e53cee3a06292bfd5d0458b7eeb610def93159fe
soggy2012 <adam.janathan@gmail.com>
2025-08-15 22:31:34 +0200

    Initial commit with Codecademy practice files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Root from './components/root';
+
+// Add react-router-dom imports
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+// create router with JSX Route elements
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={ <Root />}/>
+))
+
+function App() {
+  return (
+    // replace below with a Router Provider
+    <RouterProvider router= {appRouter} />
+  );
+}
+
+export default App;
